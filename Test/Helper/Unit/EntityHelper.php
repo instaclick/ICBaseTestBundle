087f4c982b7952f45913e049da255a1cb1c22e49
John Cartwright <johnc@nationalfibre.net>
2013-06-11 14:08:58 -0400

    Refactored Entity Helper to support optional id

--- Test/Helper/Unit/EntityHelper.php
+++ Test/Helper/Unit/EntityHelper.php
@@ -8,7 +8,8 @@ namespace IC\Bundle\Base\TestBundle\Test\Helper\Unit;
 /**
  * Entity Helper, which helps to create Entity Stubs.
  *
- * @author Guilherme Blanco <guilhermeblanco@hotmail.com> 
+ * @author Guilherme Blanco <guilhermeblanco@hotmail.com>
+ * @author John Cartwright <johnc@nationalfibre.net>
  */
 class EntityHelper extends UnitHelper
 {
@@ -23,23 +24,89 @@ class EntityHelper extends UnitHelper
      * @param string $entityClassName
      * @param mixed  $id
      */
-    public function createMock($entityClassName, $id)
+    public function createMock($entityClassName, $id = null)
     {
-        if (isset($this->identityMap[$entityClassName]) && isset($this->identityMap[$entityClassName][$id])){
-            return $this->identityMap[$entityClassName][$id];
+        if ($identity = $this->getIdentity($entityClassName, $id)) {
+            return $identity;
         }
 
+        $entity = $this->createEntityMock($entityClassName, $id);
+
+        $this->storeIdentity($entityClassName, $id, $entity);
+
+        return $entity;
+    }
+
+    /**
+     * Create the entity mock
+     *
+     * @param string $entityClassName
+     * @param mixed  $id
+     *
+     * @return object
+     */
+    private function createEntityMock($entityClassName, $id)
+    {
         $entity = $this->testCase
             ->getMockBuilder($entityClassName)
             ->setMethods(array('getId'))
             ->getMock();
-             
+
         $entity->expects($this->testCase->any())
             ->method('getId')
             ->will($this->testCase->returnValue($id));
-        
-        $this->identityMap[$entityClassName][$id] = $entity;
 
-        return $entity; 
+        return $entity;
+    }
+
+    /**
+     * Check if the id is valid.
+     *
+     * @param integer $id
+     *
+     * @return boolean
+     */
+    private function isIdentifiable($id)
+    {
+        return $id !== null;
+    }
+
+    /**
+     * Retrieve the stored reference to the mock object.
+     *
+     * @param string $entityClassName
+     * @param mixed  $id
+     *
+     * @return mixed
+     */
+    private function getIdentity($entityClassName, $id)
+    {
+        if ( ! $this->isIdentifiable($id)) {
+            return;
+        }
+
+        if ( ! isset($this->identityMap[$entityClassName]) ||  ! isset($this->identityMap[$entityClassName][$id])) {
+            return;
+        }
+
+        return $this->identityMap[$entityClassName][$id];
+    }
+
+    /**
+     * Store the reference to the mock object.
+     *
+     * @param string $entityClassName
+     * @param mixed  $id
+     * @param object $entity
+     *
+     * @return mixed
+     */
+    private function storeIdentity($entityClassName, $id, $entity)
+    {
+        if ( ! $this->isIdentifiable($id)) {
+            return;
+        }
+
+        $this->identityMap[$entityClassName][$id] = $entity;
     }
 }
